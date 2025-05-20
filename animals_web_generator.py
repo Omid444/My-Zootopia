75fa229ba4c9579b9834eabbea98ba363a6c7bd0
Omid444 <86495623+Omid444@users.noreply.github.com>
2025-05-20 19:54:47 +0200

    imprved PEP8

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,7 +1,6 @@
 import json
-
-
 from bs4 import BeautifulSoup
+
 def load_data(file_path):
   """ Loads a JSON file """
   with open(file_path, "r") as handle:
@@ -9,6 +8,7 @@ def load_data(file_path):
 
 
 def get_characteristics(animals_info):
+    """Get animals characteristics and turn it to suitable text"""
     output =''
     for animal in animals_info:
         try:
@@ -29,6 +29,7 @@ def get_characteristics(animals_info):
 
 
 def serialize_animal(animal_obj):
+    """This function support get_characteristics() to make suitable text"""
     name, diet, location, animal_type = animal_obj
     tag_start = '<li class="cards__item">'
     title = '<div class="card__title">Wire Fox Terrier</div>'
@@ -41,6 +42,7 @@ def serialize_animal(animal_obj):
 
 
 def read_html(file_path, animals_info):
+    """Read html file and return needed content"""
     with open(file_path, "r") as html_file:
         index = html_file.read()
         soup = BeautifulSoup(index, 'html.parser')
@@ -50,6 +52,7 @@ def read_html(file_path, animals_info):
 
 
 def write_html(file_path, index):
+    """Write text into html"""
     with open(file_path, "w") as html_file:
         html_file.write(index)
 
@@ -57,7 +60,6 @@ def write_html(file_path, index):
 def main():
     animals_data = load_data('animals_data.json')
     animals = get_characteristics(animals_data)
-    #print(animals)
     index = read_html('animals_template.html', animals)
     write_html('animals_template.html', index)
 
